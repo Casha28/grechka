20304ba6712cdc6d0347327b3b4f0b3ed05f247d
Casha 28 <120253367+Casha28@users.noreply.github.com>
2023-02-22 19:07:04 +0500

    Update ClientController.js

--- bek/ClientController.js
+++ bek/ClientController.js
@@ -8,8 +8,6 @@ class ClientsControler{
         try {
             await ClientService.create(req.body)
             res.status(200).json('Клиент создан')
-            // логирование
-            console.log(`client create \n ${req.body} \n`)
         } catch (error) {
             res.status(500).json('Пожалуйста заполните все поля')
             console.log(error)  
@@ -20,8 +18,6 @@ class ClientsControler{
         try {
             const clients = await ClientService.getAll()
             res.status(200).json(clients)
-            // логирование
-            console.log(`get all clients \n ${clients} \n\n clients: ${clients.length} \n`)
         } catch (error) {
             res.status(500).json(error)
             console.log(error)
@@ -32,8 +28,6 @@ class ClientsControler{
         try {
             const client = await ClientService.get(req.params.id)
             res.status(200).json(client)
-            // логирование
-            console.log(`get client ${req.params.id} \n`)
         } catch (error) {
             res.status(500).json(error)
             console.log(error)
@@ -44,8 +38,6 @@ class ClientsControler{
         try {
             const update = await ClientService.update(req.params.id, req.body)
             res.status(200).json(update)
-            // логирование
-            console.log(`client ${req.params.id} update to \n ${update} \n`)
         } catch (error) {
             res.status(500).json(error)
             console.log(error)
@@ -56,8 +48,6 @@ class ClientsControler{
         try {
             await ClientService.delete(req.params.id)
             res.status(200).json('клиент успешно удалён')
-            // логирование
-            console.log(`client ${req.params.id} delete \n`)
         } catch (error) {
             res.status(500).json('недействительный id клиента')
             console.log(error)
